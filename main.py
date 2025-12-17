a973c5ac445179d3179f6b5f4674e0519d690de6
Evgeni Atanasov <evgeniatanasov@gmail.com>
2025-12-17 20:55:09 +0200

    Fix CORS allow all origins

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # CORS Middleware - разрешава заявки от React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite default port
+    allow_origins=["*"],  # Vite default port
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
